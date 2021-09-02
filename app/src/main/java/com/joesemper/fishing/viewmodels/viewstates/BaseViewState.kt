a618eb87d5437271ea4b4682770d5231b6fac613
KostyaMerenkov <KonstantinLikes@gmail.com>
2021-09-02 18:15:58 +1000

    removed loadingProgress

--- app/src/main/java/com/joesemper/fishing/viewmodels/viewstates/BaseViewState.kt
+++ app/src/main/java/com/joesemper/fishing/viewmodels/viewstates/BaseViewState.kt
@@ -3,6 +3,5 @@ package com.joesemper.fishing.viewmodels.viewstates
 sealed class BaseViewState {
     class Success<T>(val data: T) : BaseViewState()
     class Error(val error: Throwable) : BaseViewState()
-    class LoadingProgress(val progress: Int?) : BaseViewState()
-    class Loading(val isProgress: Boolean?) : BaseViewState()
+    class Loading(val progress: Int?) : BaseViewState()
 }
