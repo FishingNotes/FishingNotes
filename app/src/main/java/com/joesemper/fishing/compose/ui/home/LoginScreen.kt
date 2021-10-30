a36f5b6358093826e7c2fe450dc9cecb3ee71faa
KostyaMerenkov <KonstantinLikes@gmail.com>
2021-10-30 14:00:48 +1000

    Added animations on login screen

--- app/src/main/java/com/joesemper/fishing/compose/ui/home/LoginScreen.kt
+++ app/src/main/java/com/joesemper/fishing/compose/ui/home/LoginScreen.kt
@@ -1,10 +1,9 @@
 package com.joesemper.fishing.compose.ui.home
 
-import android.app.Activity
 import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.*
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -29,10 +28,9 @@ import com.joesemper.fishing.compose.ui.MainDestinations
 import com.joesemper.fishing.domain.LoginViewModel
 import com.joesemper.fishing.domain.viewstates.BaseViewState
 import com.joesemper.fishing.model.entity.common.User
-import com.joesemper.fishing.ui.MainActivity
+import com.joesemper.fishing.compose.ui.MainActivity
 import com.joesemper.fishing.ui.theme.Typography
 import com.joesemper.fishing.ui.theme.primaryFigmaColor
-import org.koin.android.ext.android.get
 import org.koin.androidx.compose.get
 
 @ExperimentalMaterialApi
@@ -40,12 +38,13 @@ import org.koin.androidx.compose.get
 @Composable
 fun LoginScreen(navController: NavController) {
     var isLoading by remember { mutableStateOf(false) }
+    var visible by remember { mutableStateOf(false) }
 
     val loginViewModel: LoginViewModel = get()
     val activity = LocalContext.current as MainActivity
     val uiState = loginViewModel.subscribe().collectAsState()
 
-    LaunchedEffect(loginViewModel.subscribe().collectAsState().value) {
+    LaunchedEffect(uiState.value) {
         when (uiState.value) {
             is BaseViewState.Success<*> -> {
                 if ((uiState.value as BaseViewState.Success<*>).data as User? != null) {
@@ -56,8 +55,7 @@ fun LoginScreen(navController: NavController) {
                 }
             }
             is BaseViewState.Loading -> isLoading = true
-            is BaseViewState.Error -> {
-            }  //TODO: logger.log((uiState.value as BaseViewState.Error).error)
+            is BaseViewState.Error -> { }  //TODO: logger.log((uiState.value as BaseViewState.Error).error)
         }
     }
     //var isLoading by remember { mutableStateOf(false) }
@@ -65,88 +63,124 @@ fun LoginScreen(navController: NavController) {
     ConstraintLayout(modifier = Modifier.fillMaxSize().background(Color.White)) {
         val (background, card, lottieSuccess, cardColumn) = createRefs()
 
-        Surface(modifier = Modifier.fillMaxWidth().height(450.dp).constrainAs(background) {
-            top.linkTo(parent.top)
-        }, color = primaryFigmaColor) {}
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInVertically(
+                initialOffsetY = {
+                    // Slide in from top
+                    -it
+                },
+                animationSpec = tween(
+                    durationMillis = MainActivity.splashFadeDurationMillis*4,
 
-        Card(
+                    easing = CubicBezierEasing(0f, 0f, 0f, 1f)
+                )
+            ),
+        ) {
+            Surface(modifier = Modifier.fillMaxWidth().height(450.dp).constrainAs(background) {
+                top.linkTo(parent.top) }, color = primaryFigmaColor) {}
+        }
+
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInVertically(
+                initialOffsetY = {
+                    // Slide in from top
+                    2*it
+                },
+                animationSpec = tween(
+                    durationMillis = MainActivity.splashFadeDurationMillis*4,
+                    delayMillis = MainActivity.splashFadeDurationMillis/2,
+                    //easing = CubicBezierEasing(0f, 0f, 0f, 1f)
+
+                )
+            ),
             modifier = Modifier.constrainAs(card) {
                 top.linkTo(parent.top)
                 bottom.linkTo(parent.bottom)
-            }.fillMaxWidth().padding(30.dp).wrapContentHeight(),
-            elevation = 10.dp,
-            shape = RoundedCornerShape(30.dp)
+            }
         ) {
-
-            //LottieSuccess
-            AnimatedVisibility(false, modifier = Modifier.constrainAs(lottieSuccess) {
-                top.linkTo(card.top)
-                bottom.linkTo(card.bottom)
-                absoluteLeft.linkTo(card.absoluteLeft)
-                absoluteRight.linkTo(card.absoluteRight)
-            }) { LottieSuccess(modifier = Modifier) }
-
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .constrainAs(cardColumn) {
-                        top.linkTo(card.top)
-                        bottom.linkTo(card.bottom)
-                        absoluteLeft.linkTo(card.absoluteLeft)
-                        absoluteRight.linkTo(card.absoluteRight)
-                    }.fillMaxWidth().animateContentSize()
+            Card(
+                modifier = Modifier.fillMaxWidth().padding(30.dp).wrapContentHeight(),
+                elevation = 10.dp,
+                shape = RoundedCornerShape(30.dp)
             ) {
 
-                //AppIcon
-                Image(
-                    painterResource(R.mipmap.ic_launcher), stringResource(R.string.icon),
-                    modifier = Modifier.padding(30.dp).size(140.dp)
-                )
+                //LottieSuccess
+                AnimatedVisibility(false, modifier = Modifier.constrainAs(lottieSuccess) {
+                    top.linkTo(card.top)
+                    bottom.linkTo(card.bottom)
+                    absoluteLeft.linkTo(card.absoluteLeft)
+                    absoluteRight.linkTo(card.absoluteRight)
+                }) { LottieSuccess(modifier = Modifier) }
 
-                //Title
-                Text(
-                    stringResource(R.string.login_title),
-                    style = Typography.h5,
-                    color = Color.DarkGray
-                )
-
-                //LottieLoading
-                AnimatedVisibility(isLoading) { LottieLoading(modifier = Modifier.size(140.dp)) }
-                AnimatedVisibility(!isLoading) {
-                    Spacer(
-                        modifier = Modifier.fillMaxWidth().height(30.dp)
-                    )
-                }
-
-                //Google button
-                Card(
-                    shape = RoundedCornerShape(20.dp), elevation = 10.dp,
-                    onClickLabel = stringResource(
-                        R.string.google_login
-                    ),
-                    onClick = { isLoading = true; activity.startGoogleLogin() },
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .constrainAs(cardColumn) {
+                            top.linkTo(card.top)
+                            bottom.linkTo(card.bottom)
+                            absoluteLeft.linkTo(card.absoluteLeft)
+                            absoluteRight.linkTo(card.absoluteRight)
+                        }.fillMaxWidth().animateContentSize()
                 ) {
-                    Row(
-                        modifier = Modifier.padding(10.dp),
-                        horizontalArrangement = Arrangement.spacedBy(10.dp)
-                    ) {
-                        Image(
-                            painterResource(R.drawable.googleg_standard_color_18),
-                            stringResource(R.string.google_login), modifier = Modifier.size(25.dp)
+
+                    //AppIcon
+                    Image(
+                        painterResource(R.mipmap.ic_launcher), stringResource(R.string.icon),
+                        modifier = Modifier.padding(30.dp).size(140.dp)
+                    )
+
+                    //Title
+                    Text(
+                        stringResource(R.string.login_title),
+                        style = Typography.h5,
+                        color = Color.DarkGray
+                    )
+
+                    //LottieLoading
+                    AnimatedVisibility(isLoading) { LottieLoading(modifier = Modifier.size(140.dp)) }
+                    AnimatedVisibility(!isLoading) {
+                        Spacer(
+                            modifier = Modifier.fillMaxWidth().height(30.dp)
                         )
-                        Text("Sign in with Google", style = Typography.body1)
                     }
+
+                    //Google button
+                    Card(
+                        shape = RoundedCornerShape(20.dp), elevation = 10.dp,
+                        onClickLabel = stringResource(
+                            R.string.google_login
+                        ),
+                        onClick = { isLoading = true; activity.startGoogleLogin() },
+                    ) {
+                        Row(
+                            modifier = Modifier.padding(10.dp),
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            Image(
+                                painterResource(R.drawable.googleg_standard_color_18),
+                                stringResource(R.string.google_login),
+                                modifier = Modifier.size(25.dp)
+                            )
+                            Text("Sign in with Google", style = Typography.body1)
+                        }
+                    }
+
+                    //Space
+                    Spacer(modifier = Modifier.fillMaxWidth().height(30.dp))
                 }
 
-                //Space
-                Spacer(modifier = Modifier.fillMaxWidth().height(30.dp))
+
             }
-
-
         }
+        }
+    LaunchedEffect(true) {
+        visible = true
     }
-}
+    }
+
 
 @Composable
 private fun LottieSuccess(modifier: Modifier = Modifier) {
--- app/src/main/java/com/joesemper/fishing/di/KoinModules.kt
+++ app/src/main/java/com/joesemper/fishing/di/KoinModules.kt
@@ -8,11 +8,9 @@ import com.joesemper.fishing.model.datasource.*
 import com.joesemper.fishing.model.repository.UserContentRepository
 import com.joesemper.fishing.model.repository.UserRepository
 import com.joesemper.fishing.model.repository.WeatherRepository
-import com.joesemper.fishing.ui.MainActivity
 import com.joesemper.fishing.utils.Logger
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 
