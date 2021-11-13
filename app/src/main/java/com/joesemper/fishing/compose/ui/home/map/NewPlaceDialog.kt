2fe8deb700299c8d7fe8a27da8f5506a55a1eeaf
KostyaMerenkov <KonstantinLikes@gmail.com>
2021-11-13 20:18:16 +1000

    Added transparent marker background in dialog

--- app/src/main/java/com/joesemper/fishing/compose/ui/home/map/NewPlaceDialog.kt
+++ app/src/main/java/com/joesemper/fishing/compose/ui/home/map/NewPlaceDialog.kt
@@ -1,5 +1,7 @@
 package com.joesemper.fishing.compose.ui.home.map
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -185,16 +187,18 @@ fun NewPlaceDialog(
                     top.linkTo(description.bottom, 6.dp)
                     absoluteLeft.linkTo(parent.absoluteLeft)
                     absoluteRight.linkTo(parent.absoluteRight)
-                }.padding(horizontal = 8.dp)) {
+                }.padding(horizontal = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)) {
                 Box(
                     contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
+                    modifier = Modifier.padding(8.dp)
                         .requiredSize(40.dp))
                 {
+                    Image(painterResource(R.drawable.transparent), stringResource(R.string.transparent),)
                     Icon(painter = painterResource(id = R.drawable.ic_baseline_location_on_24),
                         contentDescription = stringResource(R.string.marker_icon),
-                        tint = selectedColor ?: secondaryFigmaColor,)
+                        tint = selectedColor ?: secondaryFigmaColor,
+                    modifier = Modifier.fillMaxSize().padding(top = 2.dp))
                 }
                 ColorPicker(
                     colors,
