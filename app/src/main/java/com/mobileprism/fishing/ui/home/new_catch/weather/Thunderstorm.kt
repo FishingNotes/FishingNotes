df5660278b55b0c26cd07a9a5f6cf1feaacc3580
Merkost <KonstantinLikes@gmail.com>
2022-03-01 22:03:08 +1000

    Thunderstorm.kt created

---
+++ app/src/main/java/com/mobileprism/fishing/ui/home/new_catch/weather/Thunderstorm.kt
@@ -0,0 +1,14 @@
+package com.mobileprism.fishing.ui.home.new_catch.weather
+
+enum class Thunderstorm {
+    ThunderstormWithLightRain,
+    ThunderstormWithRain,
+    ThunderstormWithHeavyRain,
+    LightThunderstorm,
+    Thunderstorm,
+    HeavyThunderstorm,
+    RaggedThunderstorm,
+    ThunderstormWithLightDrizzle,
+    ThunderstormWithDrizzle,
+    ThunderstormWithHeavyDrizzle
+}
