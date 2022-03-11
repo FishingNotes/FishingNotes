67b6142da38694fd15c74cec8f6b220bdd0a34b8
JoeSemper <joe.semper77@gmail.com>
2022-03-11 17:18:36 +0300

    Profile screen fix

--- app/src/main/java/com/mobileprism/fishing/ui/home/profile/Profile.kt
+++ app/src/main/java/com/mobileprism/fishing/ui/home/profile/Profile.kt
@@ -3,9 +3,7 @@ package com.mobileprism.fishing.ui.home.profile
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
@@ -26,12 +24,12 @@ import androidx.navigation.NavController
 import coil.annotation.ExperimentalCoilApi
 import com.airbnb.lottie.compose.*
 import com.mobileprism.fishing.R
+import com.mobileprism.fishing.model.entity.common.User
 import com.mobileprism.fishing.ui.Arguments
 import com.mobileprism.fishing.ui.MainDestinations
 import com.mobileprism.fishing.ui.home.views.SecondaryText
 import com.mobileprism.fishing.ui.navigate
 import com.mobileprism.fishing.ui.viewmodels.UserViewModel
-import com.mobileprism.fishing.model.entity.common.User
 import com.mobileprism.fishing.utils.time.toDateTextMonth
 import kotlinx.coroutines.InternalCoroutinesApi
 import me.vponomarenko.compose.shimmer.shimmer
@@ -51,103 +49,76 @@ fun Profile(
     val user by viewModel.currentUser.collectAsState()
 
     val imgSize: Dp = 120.dp
-    val bgHeight: Dp = 180.dp
 
-    Scaffold(modifier = modifier.fillMaxSize(),
-        topBar = { ProfileAppBar(navController) }) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = { ProfileAppBar(navController) },
+        backgroundColor = MaterialTheme.colors.primary
+    ) {
         ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
+            modifier = modifier.fillMaxSize()
         ) {
-            val (background, card, image, name, places, catches, registerDate) = createRefs()
-
-            val bgGl = createGuidelineFromTop(80.dp)
-            val verticalCenterGl = createGuidelineFromAbsoluteLeft(0.5f)
+            val (image, card) = createRefs()
 
             val placesState by viewModel.currentPlaces.collectAsState()
             val catchesState by viewModel.currentCatches.collectAsState()
 
-            Surface(//shape = RoundedCornerShape(0.dp,0.dp,15.dp,15.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(bgHeight)
-                    .constrainAs(background) {
-                        top.linkTo(parent.top)
-                        absoluteLeft.linkTo(parent.absoluteLeft)
-                        absoluteRight.linkTo(parent.absoluteRight)
-                    },
-                color = MaterialTheme.colors.primary
-            ) {}
-
             UserImage(user, imgSize, modifier = Modifier
                 .constrainAs(image) {
-                    top.linkTo(parent.top)
                     absoluteLeft.linkTo(parent.absoluteLeft)
                     absoluteRight.linkTo(parent.absoluteRight)
-                    bottom.linkTo(parent.bottom)
-                    centerAround(bgGl)
+                    centerAround(card.top)
                 }
                 .zIndex(2f))
-
-            UserText(user, modifier = Modifier
-                .constrainAs(name) {
-                    top.linkTo(image.bottom)
-                    absoluteLeft.linkTo(card.absoluteLeft)
-                    absoluteRight.linkTo(card.absoluteRight)
-                }
-                .zIndex(2f))
-
-            SecondaryText(
-                modifier = Modifier
-                    .constrainAs(registerDate) {
-                        top.linkTo(name.bottom, 2.dp)
-                        absoluteLeft.linkTo(name.absoluteLeft)
-                        absoluteRight.linkTo(name.absoluteRight)
-                    }
-                    .zIndex(2f),
-                text = "${stringResource(id = R.string.register_date)}: " +
-                        (user?.registerDate?.toDateTextMonth() ?: "")
-            )
 
             Card(
                 modifier = Modifier
                     .constrainAs(card) {
-                        top.linkTo(bgGl)
+                        top.linkTo(parent.top, imgSize)
+                        bottom.linkTo(parent.bottom)
                         absoluteLeft.linkTo(parent.absoluteLeft)
                         absoluteRight.linkTo(parent.absoluteRight)
                     }
-                    .fillMaxWidth()
-                    .wrapContentHeight()
+                    .fillMaxSize()
                     .zIndex(1f),
                 shape = AbsoluteRoundedCornerShape(25.dp, 25.dp),
-                elevation = 10.dp,
+                elevation = 8.dp,
                 backgroundColor = MaterialTheme.colors.surface
             ) {
-                Divider()
-                Column(
-                    modifier = Modifier
-                        .padding(top = 120.dp, bottom = 120.dp)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
+                ConstraintLayout(
+                    modifier = Modifier.fillMaxSize(),
                 ) {
-                    //val bestCatch by viewModel.bestCatch.collectAsState()
                     val favoritePlace by viewModel.favoritePlace.collectAsState()
 
-                    /*BestCatchView(
-                        bestCatch = bestCatch,
-                        onCatchItemClick = {
-                            navController.navigate(
-                                MainDestinations.CATCH_ROUTE,
-                                Arguments.CATCH to it
-                            )
-                        }
-                    )*/
+                    val (name, places, catches, registerDate, favorite) = createRefs()
+                    val verticalCenterGl = createGuidelineFromAbsoluteLeft(0.5f)
 
-                    //Spacer(modifier = Modifier.size(16.dp))
+                    UserText(user, modifier = Modifier
+                        .constrainAs(name) {
+                            top.linkTo(parent.top, (imgSize / 2 + 8.dp))
+                            absoluteLeft.linkTo(parent.absoluteLeft)
+                            absoluteRight.linkTo(parent.absoluteRight)
+                        }
+                        .zIndex(2f))
+
+                    SecondaryText(
+                        modifier = Modifier
+                            .constrainAs(registerDate) {
+                                top.linkTo(name.bottom, 2.dp)
+                                absoluteLeft.linkTo(name.absoluteLeft)
+                                absoluteRight.linkTo(name.absoluteRight)
+                            }
+                            .zIndex(2f),
+                        text = "${stringResource(id = R.string.register_date)}: " +
+                                (user?.registerDate?.toDateTextMonth() ?: "")
+                    )
 
                     FavoritePlaceView(
+                        modifier = Modifier.constrainAs(favorite) {
+                            top.linkTo(registerDate.bottom, 32.dp)
+                            absoluteLeft.linkTo(parent.absoluteLeft)
+                            absoluteRight.linkTo(parent.absoluteRight)
+                        },
                         favoritePlace = favoritePlace,
                         userPlaceClicked = {
                             navController.navigate(
@@ -162,34 +133,31 @@ fun Profile(
                             )
                         }
                     )
+
+                    PlacesNumber(
+                        modifier = Modifier
+                            .constrainAs(places) {
+                                top.linkTo(parent.top, 16.dp)
+                                absoluteLeft.linkTo(verticalCenterGl, imgSize / 2)
+                                absoluteRight.linkTo(parent.absoluteRight)
+                            }
+                            .zIndex(3f),
+                        userPlacesNum = placesState?.size
+                    )
+
+                    CatchesNumber(
+                        modifier = Modifier
+                            .constrainAs(catches) {
+                                top.linkTo(parent.top, 16.dp)
+                                absoluteLeft.linkTo(parent.absoluteLeft)
+                                absoluteRight.linkTo(verticalCenterGl, imgSize / 2)
+                            }
+                            .zIndex(3f),
+                        userCatchesNum = catchesState?.size
+                    )
                 }
             }
-
-            PlacesNumber(
-                modifier = Modifier
-                    .constrainAs(places) {
-                        top.linkTo(bgGl)
-                        absoluteLeft.linkTo(verticalCenterGl, imgSize / 2)
-                        absoluteRight.linkTo(parent.absoluteRight)
-                        bottom.linkTo(image.bottom)
-                    }
-                    .zIndex(3f),
-                userPlacesNum = placesState?.size
-            )
-
-            CatchesNumber(
-                modifier = Modifier
-                    .constrainAs(catches) {
-                        top.linkTo(bgGl)
-                        absoluteLeft.linkTo(parent.absoluteLeft)
-                        absoluteRight.linkTo(verticalCenterGl, imgSize / 2)
-                        bottom.linkTo(image.bottom)
-                    }
-                    .zIndex(3f),
-                userCatchesNum = catchesState?.size
-            )
         }
-
     }
 }
 
