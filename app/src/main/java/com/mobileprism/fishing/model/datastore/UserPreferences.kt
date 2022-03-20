c26fabe612443f8ccc82c8e873e4526dcb8efb4e
Merkost <KonstantinLikes@gmail.com>
2022-03-20 13:14:36 +1000

    Fixed first enter zoom

--- app/src/main/java/com/mobileprism/fishing/model/datastore/UserPreferences.kt
+++ app/src/main/java/com/mobileprism/fishing/model/datastore/UserPreferences.kt
@@ -72,7 +72,7 @@ class UserPreferences(private val context: Context) {
                     preferences[LAST_MAP_LATITUDE] ?: 0.0,
                     preferences[LAST_MAP_LONGITUDE] ?: 0.0,
                 ),
-                preferences[LAST_MAP_ZOOM] ?: DEFAULT_ZOOM,
+                preferences[LAST_MAP_ZOOM] ?: 0f,
                 preferences[LAST_MAP_BEARING] ?: 0f,
                 )
         }
