64389c07db6d9943a92c20647dffc7c76f9aa17d
Konstantin Merenkov <56008710+Merkost@users.noreply.github.com>
2022-09-28 15:14:04 +1000

    Created FishingOutlinedTextField

--- app/src/main/java/com/mobileprism/fishing/ui/custom/TextFields.kt
+++ app/src/main/java/com/mobileprism/fishing/ui/custom/TextFields.kt
@@ -1,16 +1,19 @@
 package com.mobileprism.fishing.ui.custom
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -31,23 +34,45 @@ fun FishingOutlinedTextField(
     singleLine: Boolean = false,
     maxLines: Int = Int.MAX_VALUE,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors()
+    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(),
+    errorString: String? = null,
 ) {
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = value,
-        onValueChange = onValueChange,
-        shape = RoundedCornerShape(12.dp),
-        label = { placeholder?.let { Text(text = it) } },
-        keyboardOptions = keyboardOptions,
-        keyboardActions =  keyboardActions,
-        enabled = enabled,
-        singleLine = singleLine,
-        maxLines = maxLines,
-        readOnly = readOnly,
-        isError = isError,
-        visualTransformation = visualTransformation,
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon
-    )
+    Column(modifier = Modifier) {
+        OutlinedTextField(
+            modifier = modifier,
+            value = value,
+            onValueChange = onValueChange,
+            shape = RoundedCornerShape(12.dp),
+            maxLines = maxLines,
+            label = {
+                placeholder?.let {
+                    Text(
+                        text = it,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = maxLines,
+                    )
+                }
+            },
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            enabled = enabled,
+            singleLine = singleLine,
+            readOnly = readOnly,
+            isError = isError,
+            visualTransformation = visualTransformation,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            interactionSource = interactionSource,
+            colors = colors,
+            textStyle = textStyle,
+        )
+        AnimatedVisibility(visible = errorString.isNullOrBlank().not()) {
+            Text(
+                text = errorString ?: "",
+                color = MaterialTheme.colors.error,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
+    }
 }
+
