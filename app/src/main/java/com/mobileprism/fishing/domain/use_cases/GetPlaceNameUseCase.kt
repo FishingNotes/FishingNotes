779f9e5bb00aa116a004d7209d86a95fc00d19e3
Konstantin Merenkov <56008710+Merkost@users.noreply.github.com>
2022-09-30 12:42:46 +1000

    GetPlaceNameUseCase refactoring

--- app/src/main/java/com/mobileprism/fishing/domain/use_cases/GetPlaceNameUseCase.kt
+++ app/src/main/java/com/mobileprism/fishing/domain/use_cases/GetPlaceNameUseCase.kt
@@ -1,27 +1,45 @@
 package com.mobileprism.fishing.domain.use_cases
 
+import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import com.mobileprism.fishing.ui.home.map.GeocoderResult
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 
 class GetPlaceNameUseCase(private val geocoder: Geocoder) {
 
     suspend operator fun invoke(
         latitude: Double,
         longitude: Double,
-    ) = flow {
-
+    ) = callbackFlow {
         try {
-            val position = geocoder.getFromLocation(latitude, longitude, 1)
-            position?.first()?.let { address ->
-                if (!address.subAdminArea.isNullOrBlank()) {
-                   emit(GeocoderResult.Success( address.subAdminArea.replaceFirstChar { it.uppercase() }))
-                } else if (!address.adminArea.isNullOrBlank()) {
-                    emit(GeocoderResult.Success(address.adminArea.replaceFirstChar { it.uppercase() }))
-                } else emit(GeocoderResult.NoNamePlace)
+            if (Build.VERSION.SDK_INT >= 33) {
+                geocoder.getFromLocation(latitude, longitude, 1) {
+                    it.firstOrNull()?.let { address ->
+                        trySend(address.getAddressName())
+                    }
+                }
+            } else {
+                val position = geocoder.getFromLocation(latitude, longitude, 1)
+                position?.firstOrNull()?.let { address ->
+                    trySend(address.getAddressName())
+                } ?: trySend(GeocoderResult.NoNamePlace)
             }
         } catch (e: Throwable) {
-            emit(GeocoderResult.Failed)
+            send(GeocoderResult.Failed)
         }
+
+        awaitClose {}
+    }
+
+    private fun Address.getAddressName(): GeocoderResult {
+        return if (!this.subAdminArea.isNullOrBlank()) {
+            GeocoderResult.Success(this.subAdminArea.replaceFirstChar { it.uppercase() })
+        } else if (!this.adminArea.isNullOrBlank()) {
+            GeocoderResult.Success(this.adminArea.replaceFirstChar { it.uppercase() })
+        } else GeocoderResult.NoNamePlace
     }
 }
+
+
